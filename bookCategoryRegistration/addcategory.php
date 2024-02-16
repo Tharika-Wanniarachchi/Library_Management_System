a5aff336c4ce9d6d39b6d70bb6d96051030f84eb
Nuwandi-Dulshara <dulshar-ct19047@stu.kln.ac.lk>
2024-02-16 19:13:36 +0530

    addcategory commit
    add addcategory file to book registration folder

---
+++ bookCategoryRegistration/addcategory.php
@@ -0,0 +1,21 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Assign Fine</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+</head>
+<body>
+    
+    <!-- Bootstrap JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-KyZXEAg3QhqLMpG8r+Knujsl5+2vx2KPScO2L8Lw5VW49pJfNOQIkFwJ8JwCz3Z6" crossorigin="anonymous"></script>
+
+    
+</body>
+</html>
