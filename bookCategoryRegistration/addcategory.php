ef77a929bd7d20032dc59133ef1366a7544a9985
Nuwandi-Dulshara <152952037+Nuwandi-Dulshara@users.noreply.github.com>
2024-02-16 23:40:08 +0530

    Update addcategory.php

--- bookCategoryRegistration/addcategory.php
+++ bookCategoryRegistration/addcategory.php
@@ -36,7 +36,7 @@ if(isset($_POST['submit'])){
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Assign Fine</title>
+    <title>Add Book Category </title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
