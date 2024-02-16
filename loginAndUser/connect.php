fc442604cccdad1ee5226d8730ea4a105d7cdda6
SudeshMaduwantha <sudeshmaduwantha204@gmail.com>
2024-02-16 12:41:41 +0530

    Establish database connection to php files

---
+++ loginAndUser/connect.php
@@ -0,0 +1,10 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "library_test";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+?>
