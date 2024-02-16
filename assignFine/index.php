0729a3bc47f0f7d3f54ce3fbe18f37597c6f53d3
Tharika-Wanniarachchi <147181160+Tharika-Wanniarachchi@users.noreply.github.com>
2024-02-16 22:56:19 +0530

    add assign Fine

---
+++ assignFine/index.php
@@ -0,0 +1,65 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assign fine</title>
+
+    <link rel="stylesheet" href="style.css">
+     <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    
+</head>
+<body>
+
+    <div class="container">
+        <div class="card my-3 px-3" style="background:#9f9ee7a2;">
+                <div  class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <button class="btn btn-secondary my-3"><a href="../loginAndUser/homeInterface.php" class="text-light btn_text">Back to home</a></button>
+                </div>
+            <div class="card-body">
+                <div class="card-header" style="background:#1B1A55 ; color:white;" >
+                    <h1 class="text-center">Assign Fine For a User<img class="mx-5" src="../img/assign-white.png" alt="bookreg" width="60px" height=""></h1>
+                </div>
+                <button class="btn btn-dark mt-5 "><a href="addbook.php" class="text-light btn_text"><a href="assignfine.php" class="text-light btn_text"><img src="../img/assign-white.png" alt="addbook"  width="20px"> Assign Fine</a></button>
+
+                
+                <table class="table table-bordered">
+                    <thead class="table-light">
+                        <tr>
+                            <th class="p-3" scope="col">Fine ID</th>
+                            <th class="p-3"scope="col">Member ID</th>
+                            <th class="p-3"scope="col">Member Name</th>
+                            <th class="p-3"scope="col">Book name related to relevant fine</th>
+                            <th class="p-3"scope="col">Fine amount in LKR</th>
+                            <th class="p-3"scope="col">Date Modified </th>
+                            <th class="p-3"scope="col">Operation </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                       
+                                    
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    
+     <!--Boostrap Jquery-->
+
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+    <!--Boostrap Javascript-->
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+
+
+
+    
+</body>
+</html>
